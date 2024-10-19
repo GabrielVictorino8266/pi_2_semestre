179cec1c224800c39affddcbabc225656fd4fc19
GabrielVictorino8266 <gabriel.victorino2004@gmail.com>
2024-10-19 08:29:12 -0300

    feat: Adicionado tabelas de usuarios e funções no SQL para controle de permissão.

--- sql/database.sql
+++ sql/database.sql
@@ -1,2 +1,17 @@
 CREATE DATABASE rotisdb;
 USE rotisdb;
+
+
+CREATE TABLE IF NOT EXISTS tb_funcoes(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    descricao VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS tb_usuarios(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(50) NOT NULL,
+    senha VARCHAR(255) NOT NULL,
+    fk_funcao_id INT NOT NULL,
+    FOREIGN KEY fk_funcao_id REFERENCES tb_funcoes(id)
+
+);
