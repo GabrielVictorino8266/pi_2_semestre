184cbeccaea07052860bbada80d1c9fce67548f8
GabrielVictorino8266 <gabriel.victorino2004@gmail.com>
2024-11-15 19:10:58 -0300

    fix: Corrigindo importação em ctr_dashboard

--- php/ctr_dashboard.php
+++ php/ctr_dashboard.php
@@ -1,7 +1,7 @@
 <?php
-require_once __DIR__ . './classes/dashboard.php';
-require_once __DIR__ . './classes/paginacao.php';
-require_once __DIR__ . './session_check.php';
+require_once __DIR__ . '/classes/dashboard.php';
+require_once __DIR__ . '/classes/paginacao.php';
+require_once __DIR__ . '/session_check.php';
 
 define('PROJECT_ROOT_MYPATH', '../view'); // Ajuste para o caminho da raiz do projeto, como '/' para a raiz ou '/meu_projeto/'
 verificarSessao(PROJECT_ROOT_MYPATH);
