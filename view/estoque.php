67c5c4018a5dd818ef5fa8b522b52f4011af0177
GabrielVictorino8266 <gabriel.victorino2004@gmail.com>
2024-11-24 01:19:05 -0300

    style: aplicando bootstrap.

--- view/estoque.php
+++ view/estoque.php
@@ -11,6 +11,8 @@ require_once __DIR__ . '../../php/ctr_estoque.php';
     <title>Estoque</title>
     <link rel="stylesheet" href="../style/style.css">
     <script src="../js/estoque.js" defer></script>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
     <!-- NAVBAR -->
@@ -20,7 +22,7 @@ require_once __DIR__ . '../../php/ctr_estoque.php';
                 <div>
                     <p>Olá, <?php echo $_SESSION['user_name']; ?></p>
                 </div>
-                <a href="./logout.php">Sair</a>
+                <a class="link-opacity-75" href="./logout.php">Sair</a>
             </div>
             <div>
                 <?php
@@ -241,5 +243,6 @@ require_once __DIR__ . '../../php/ctr_estoque.php';
                 }
             ?>
         </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
